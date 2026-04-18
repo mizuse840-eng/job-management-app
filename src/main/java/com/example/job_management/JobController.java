d8dce7fa9ab99e87be6fafc4064521b36f875c01
mizuse840-eng <mizuse840@gmail.com>
2026-04-18 17:16:26 +0900

    rename: 実態に合わせてHelloControllerをJobControllerに名前変更

--- src/main/java/com/example/job_management/JobController.java
+++ src/main/java/com/example/job_management/JobController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Controller
-public class HelloController {
+public class JobController {
 
     @Autowired
     private JobRepository jobRepository;
